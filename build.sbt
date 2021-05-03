e7da696b89c1b98d6c5de59125d3c8ff532c82b1
jiandong <jiandong.chen@outlook.com>
2021-05-03 16:06:06 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "jsonflatter"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
+
+useCoursier := false
---
+++ src/main/java/antlr/JsonVisitor.java
@@ -0,0 +1,45 @@
+// Generated from Json.g4 by ANTLR 4.9
+
+    package antlr;
+
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link JsonParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface JsonVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link JsonParser#json}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitJson(JsonParser.JsonContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link JsonParser#obj}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitObj(JsonParser.ObjContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link JsonParser#pair}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPair(JsonParser.PairContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link JsonParser#arr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArr(JsonParser.ArrContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link JsonParser#value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitValue(JsonParser.ValueContext ctx);
+}
