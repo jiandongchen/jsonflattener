dee0fcabaaf395bbcd89fb675c4a2953204dfa94
jiandong <jiandong.chen@outlook.com>
2021-05-05 17:26:08 +0800

    updated code

--- src/main/scala/app/JsonApp.scala
+++ src/main/scala/app/JsonApp.scala
@@ -8,13 +8,18 @@ import org.antlr.v4.runtime.{CharStreams, CommonTokenStream}
 
 object JsonApp {
   def main(args: Array[String]): Unit = {
-    val fileName = "F:\\Idea Projects\\jsonflatter\\src\\main\\java\\tests\\test1.json"
-    val parser = getParser(fileName)
-    val antlrAST = parser.json()
-    val jsonVisitor = new AntlrToJson()
-    val json: Json = jsonVisitor.visit(antlrAST)
-    val flattenedJson = JsonFlatter.flatJson(json)
-    println(flattenedJson.toString)
+    if (args.length != 1) {
+      System.err.println("Usage: file name")
+    }
+    else {
+      val fileName = args(0)
+      val parser = getParser(fileName)
+      val antlrAST = parser.json()
+      val jsonVisitor = new AntlrToJson()
+      val json: Json = jsonVisitor.visit(antlrAST)
+      val flattenedJson = JsonFlatter.flatJson(json)
+      println(flattenedJson.toString)
+    }
   }
 
   private def getParser(fileName: String): JsonParser = {
